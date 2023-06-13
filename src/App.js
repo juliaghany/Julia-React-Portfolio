795bad72c5471f39d0d5fdd1015ccba8ee29746c
Jules <juliaghany@msn.com>
2023-06-13 12:01:05 -0500

    initial setup with App component and PortfolioContainer

---
+++ src/App.js
@@ -0,0 +1,7 @@
+// Referring to Activity 24 -> src -> App.js
+import React from 'react';
+import PortfolioContainer from './components/PortfolioContainer'
+
+const App = () => <PortfolioContainer />;
+
+export default App;
