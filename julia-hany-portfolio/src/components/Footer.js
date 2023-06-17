e6e293b15727bc043284c76ebc6065ba6a236e11
Jules <juliaghany@msn.com>
2023-06-17 10:23:00 -0500

    create footer component, incldue links to github, linkedin, and instagram using icons

--- julia-hany-portfolio/src/components/Footer.js
+++ julia-hany-portfolio/src/components/Footer.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa'
 
 const styles = {
     background: {
         backgroundColor: 'black'
+    },
+    iconContainer: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    icons: {
+        fontSize: '40px',
+        margin: '10px'
     }
 }
 
 const Footer = () => {
     return (
-        <footer style={styles.background}>This is the footer</footer>
+        <footer style={styles.background}>
+            <div style={styles.iconContainer}>
+                <a href='https://www.linkedin.com/in/julia-hany-41224626b/' target='_blank'>
+                    <FaLinkedin style={styles.icons} />
+                </a>
+                <a href='https://github.com/juliaghany' target='_blank'>
+                    <FaGithub style={styles.icons} />
+                </a>
+                <a href='https://www.instagram.com/heyimjules/' target='_blank'>
+                    <FaInstagram style={styles.icons} />
+                </a>
+            </div>
+        </footer>
     );
-} 
+}
 
 export default Footer;
