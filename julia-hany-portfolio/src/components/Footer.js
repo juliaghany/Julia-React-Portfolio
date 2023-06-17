9f59e5486731e37946f7d81145d118efb872247e
Jules <juliaghany@msn.com>
2023-06-17 11:18:20 -0500

    removed instagram icon, replaced with mail icon so user can email me if they would like it. Also added titles to the icons for tooltips when user hovers over them

--- julia-hany-portfolio/src/components/Footer.js
+++ julia-hany-portfolio/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa'
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'
 
 const styles = {
     background: {
@@ -19,14 +19,14 @@ const Footer = () => {
     return (
         <footer style={styles.background}>
             <div style={styles.iconContainer}>
-                <a href='https://www.linkedin.com/in/julia-hany-41224626b/' target='_blank'>
+                <a href='https://www.linkedin.com/in/julia-hany-41224626b/' title='View my LinkedIn' target='_blank'>
                     <FaLinkedin style={styles.icons} />
                 </a>
-                <a href='https://github.com/juliaghany' target='_blank'>
+                <a href='https://github.com/juliaghany' title='View my GitHub' target='_blank'>
                     <FaGithub style={styles.icons} />
                 </a>
-                <a href='https://www.instagram.com/heyimjules/' target='_blank'>
-                    <FaInstagram style={styles.icons} />
+                <a href='mailto:juliaghany@msn.com' title='Email me' target='_blank'>
+                    <FaEnvelope style={styles.icons} />
                 </a>
             </div>
         </footer>
