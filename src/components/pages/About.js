6e90b429a1bac0c98fb6e219d072bcb932b1c585
Jules <juliaghany@msn.com>
2023-06-23 11:39:26 -0500

    Updates

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -28,13 +28,7 @@ export default function About() {
                 I have nearly completed a comprehensive web development course through Columbia Engineering Boot Camp where I've gained a solid 
                 foundation in front-end and back-end technologies. Prior to that, I gained experience as a waitress, honing my communication
                 and time management skills in a fast-paced environment. Working closely with diverse guests, I developed exceptional interpersonal 
-                skills, effectively listening to their needs and providing great service. This experience taught me the importance of clear and 
-                concise communication, adaptability, and working collaboratively as part of a team.<br/><br/>
-
-                I have hands-on experience with HTML, CSS, JavaScript, and I have worked with popular frameworks such as React and Node.js.
-                I'm also skilled in version control using Git and have a basic understanding of databases and SQL.
-                During the web development course, I completed several projects that allowed me to apply my skills in real-world scenarios.
-                One of my notable projects was building a dynamic website that enables users to search for movies, discover recommended films, 
+               
                 and create personalized movie lists.<br/><br/>
 
                 Outside of coding, you can find me exploring nature on hiking trails, expressing my creativity through various forms of art, 
