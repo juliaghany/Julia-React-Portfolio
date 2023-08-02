644016d25cf837a152ecdd5d12e70bbda3234e8f
Jules <juliaghany@msn.com>
2023-08-02 12:03:21 -0400

    add summary of project and technologies used to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -20,6 +20,8 @@ const projects = [
         link2: 'https://birthday-buddy.herokuapp.com/login',
         image: birthdayBuddyImage,
         alt: 'Birthday confetti (purple, pink, teal, gold) with white background',
+        text: 'BirthdayBuddy is an application that allows users to create a list of gift ideas for friends and family. The user can add a receiver, their name and their birthday, and then they are able to create and view a list of gift ideas for that receiver.',
+        technologies: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Express', 'Handlebars', 'MySQL']
     },
     {
         name: 'Popcorn Portal',
@@ -27,6 +29,8 @@ const projects = [
         link2: 'https://juliaghany.github.io/Popcorn-Portal/',
         image: popcornProjectImage,
         alt: 'Buttery popcorn in movie theater bucket with red and white stripes, logo on the bucket that reads "Popcorn Portal"',
+        text: 'Popcorn Portal is an application created for users to search and save movies. After searching and selecting a movie, the user will then be presented with information such as: the movie poster, director, actors, description, streaming platforms, and recommended movies.',
+        technologies: ['HTML', 'CSS', 'JavaScript']
     },
     {
         name: 'JamVault',
@@ -34,6 +38,8 @@ const projects = [
         link2: 'https://jamvault-3a4f37943c6d.herokuapp.com/',
         image: jamVault,
         alt: 'Pink, purple, orange header and footer. JamVault as title, links in navigation bar read The Vault, Timeline, and Login or Logout',
+        text: 'An application where users can search, select, and share memories from a concert. After sharing their own memories, users can view other posts made by other users.',
+        technologies: ['React', 'CSS', 'JavaScript', 'React Router DOM', 'Bootstrap', 'Node.js', 'Express'],
     },
     {
         name: 'Tech Blog',
@@ -41,6 +47,8 @@ const projects = [
         link2: 'https://julia-hany-tech-blog.herokuapp.com/',
         image: techBlogImage,
         alt: 'Blog website that has a home, dashboard, and login tabs. Shows title of blog, username of who posted it, date created, and blog content',
+        text: 'Tech Blog is a a CMS-style blog site where users can publish articles, blog posts, and their thoughts and opinions on tech topics.',
+        technologies: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Express.js', 'MySQL'],
 
     },
     {
@@ -49,6 +57,8 @@ const projects = [
         link2: 'https://juliaghany.github.io/Weather-Dashboard/',
         image: weatherDashboardImage,
         alt: 'Weather dashboard that shows search bar, once a city is searched it shows the temperature, wind speed, and humidity for next five days for that city',
+        text: 'Weather Dashboard is an application that allows users to search and view the 5-day forecast for a city of their choosing.',
+        technologies: ['HTML', 'CSS', 'JavaScript']
     },
     {
         name: 'Timed Code Quiz',
@@ -56,6 +66,8 @@ const projects = [
         link2: 'https://juliaghany.github.io/Timed-Code-Quiz/',
         image: quizImage,
         alt: 'Introduction page to a coding quiz challenge, start quiz button at the bottom',
+        text: 'Timed Code Quiz is an application where users can test their knowledge on coding by taking a timed, multiple-choice questions quiz.',
+        technologies: ['HTML', 'CSS', 'JavaScript']
     }
 ]
 // display each project 
@@ -76,7 +88,7 @@ export default function Portfolio() {
                                 alt={project.alt}
                             />
                             <div className="portfolio-overlay">
-                                <a className="portfolio-name" href={project.link2} target='_blank'>{project.name}</a>
+                                <a className="portfolio-name" href={project.link2} target='_blank' style={{ paddingTop: '5px'}}>{project.name}</a>
                                 <a
                                     className="portfolio-link"
                                     href={project.link1}
@@ -85,6 +97,14 @@ export default function Portfolio() {
                                 >
                                     <FaGithub className="github-icon" />
                                 </a>
+                                <p style={{ color: 'white', paddingTop: '7px', textAlign: 'center' }}>{project.text}</p>
+                                {project.technologies?.length ? (
+                                    <ul style={{ color: 'white'}}>
+                                        {project.technologies.map((technology, index) => (
+                                            <li key={index}>{technology}</li>
+                                        ))}
+                                    </ul>
+                                ) : null}
                             </div>
                         </motion.div>
                     </div>
